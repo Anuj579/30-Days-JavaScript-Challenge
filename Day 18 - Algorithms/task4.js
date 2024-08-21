f46d1f08d3bcc5cffc1da04e1d942aedcb4e126e
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-08-21 22:19:31 +0530

    Day 18 - Algorithms

--- Day 18 - Algorithms/task4.js
+++ Day 18 - Algorithms/task4.js
@@ -1,2 +1,16 @@
 // Activity 2: Searching Algorithms - task4 to task5
 
+function linearSearch(arr, target) {
+    for (let i = 0; i < arr.length; i++){
+        if (arr[i] === target) {
+            return `Element found at index: ${i}`
+        }
+    }
+    return `Element not found`
+}
+
+let arr = [2,34,5,6,53]
+let target = 53
+
+linearSearch(arr, target);
+console.log(linearSearch(arr, target));
---
+++ Day 18 - Algorithms/task5.js
@@ -0,0 +1,26 @@
+function binarySearch(arr, target) {
+    
+    let left = 0;
+    let right = arr.length - 1
+
+    while (left <= right) {
+        const middle = Math.floor((left + right) / 2)
+
+        if (arr[middle] === target) {
+            return middle
+        } else if (arr[middle] < target) {
+            left = middle + 1
+        } else {
+            right = middle - 1
+        }
+    }
+    return `Element not found`
+}
+
+let arr = [2,3,4,6,7]
+console.log(`Array: ${arr}`);
+
+let target = 6
+
+const index = binarySearch(arr.sort(), target)
+console.log(`Element ${target} is found at index: ${index}`);
