b733ff07ab17d539a3d7ac9325a6d264ad60b429
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-09-02 18:32:44 +0530

    Day 22 - LeetCode Medium

--- Day 22 - LeetCode Medium/task5.js
+++ Day 22 - LeetCode Medium/task5.js
@@ -1,2 +1,21 @@
 // Activity 5: Group Anagrams
 // Task 5 - Write a function that takes an array of strings and groups anagrams together. Log the grouped anagram for few test cases.
+
+function groupAnagrams(strs){
+    let groups ={}
+
+    for (let str of strs){
+        let sortedStr = str.split('').sort().join('')
+        
+        if(!groups[sortedStr]){
+            groups[sortedStr] = []
+        }
+
+        groups[sortedStr].push(str)
+    }
+
+    return Object.values(groups)
+}
+
+console.log(groupAnagrams(['low', 'owl', 'ate', 'tea', 'tan']));
+console.log(groupAnagrams(['tab', 'bat', 'who', 'how']));
