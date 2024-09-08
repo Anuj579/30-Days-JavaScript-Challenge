3e200730e9eea77ea7d1312e3f17bdc236e70452
Anuj Chaudhary <alwaysmile9813@gmail.com>
2024-09-08 23:06:41 +0530

    LeetCode hard

---
+++ Day 23 - LeetCode Hard/task2.js
@@ -0,0 +1,3 @@
+// Activity 2: Merge k Sorted List
+// Task 2 - Write a function that takes an array of k linked lists, each linked list is sorted in ascending order, and merges all linked lists into one sorted list. Create a few test cases with linked lists and log the merged list.
+
